415106994cb413bde7996aa4d0f4cec9d585fb82
DuongLN <leduong1998st@gmail.com>
2023-05-24 12:44:10 +0700

    Add Next.js build files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/nextjs-e-commerce-fullstack-mongo-tallwinds',
+  assetPrefix: '/nextjs-e-commerce-fullstack-mongo-tallwinds/'
 }
 
 module.exports = nextConfig
